a669541ab6d92e35e7b66c9baebe3edab7550a39
Jaime Orlando Lopez Ramos <30807749+JaimeOrlandoLpz@users.noreply.github.com>
2021-10-24 19:34:08 -0500

    useState

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+  // define state variable and state changing function
+  const [title, setTitle]=useState(props.title);
+
+  // Function to react to click event
+  const clickHandler = ()=>{
+    // Update title using the state hook, so page re-renders
+    setTitle("Hieeee");
+  }
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        {/* We access the dynamic title value */}
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Click me</button>
     </Card>
   );
 }
