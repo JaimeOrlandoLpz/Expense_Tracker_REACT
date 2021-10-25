72392e131cafbbca46a45842c4de544fc1b617f5
Jaime Orlando Lopez Ramos <30807749+JaimeOrlandoLpz@users.noreply.github.com>
2021-10-24 20:48:34 -0500

    Input Listener

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,13 +3,18 @@ import "./ExpenseForm.css"
 
 // This Component will handle the form logic specifically
 const ExpenseForm = () => {
+
+    // Function to be executed every time title input changes
+    const titleChangeHandler = (event) =>{
+        console.log(event.target.value);
+    }
     return(
         <form>
 
             <div className = "new-expense__controls">
                 <div className = "new-expense__control">
                     <label>Title: </label>
-                    <input type="text"/>
+                    <input type="text" onChange={titleChangeHandler}/>
                 </div>
 
                 <div className = "new-expense__control">
