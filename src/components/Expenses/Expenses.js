68d30ac199e3d22042fa8924b3e42a06986a6416
Jaime Orlando Lopez Ramos <30807749+JaimeOrlandoLpz@users.noreply.github.com>
2021-11-05 11:11:54 -0600

    Dynamic List Rendering

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -18,7 +18,21 @@ const Expenses = (props) => {
     <>
       <ExpensesFilter onChangeFilter = {filterChangedHandler} selected = {filteredYear}></ExpensesFilter>
       <Card className="expenses">
-        <ExpenseItem
+
+      { // Dynamically rendering our array
+          props.items.map((expense)=>{
+            return(
+              <ExpenseItem 
+                title = {expense.title}
+                amount = {expense.amount}
+                date = {expense.date}
+              />
+            );
+        })
+      }
+
+       { // STATIC RENDERING (NOT GOOD)
+         /* <ExpenseItem
           title={props.items[0].title}
           amount={props.items[0].amount}
           date={props.items[0].date}
@@ -37,7 +51,7 @@ const Expenses = (props) => {
           title={props.items[3].title}
           amount={props.items[3].amount}
           date={props.items[3].date}
-        />
+        /> */}
       </Card>
     </>
   );
